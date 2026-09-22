b1422d5b9edebf632d0786603adfd132f45bf6ad
Shyam Kumar R <shyamkumareec03@gmail.com>
2026-09-22 11:58:46 +0530

    fix: replace CARTO watermark tiles with watermark-free Esri World Dark Gray Canvas

--- frontend/src/components/RoutePlanner.jsx
+++ frontend/src/components/RoutePlanner.jsx
@@ -472,8 +472,9 @@ export default function RoutePlanner({ activeVehicle, stations = [], onClose, on
               zoomControl={true}
             >
               <TileLayer
-                url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-                attribution='&copy; <a href="https://carto.com/">CARTO</a>'
+                url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+                attribution='&copy; <a href="https://www.esri.com/">Esri</a> &copy; OpenStreetMap'
+                maxZoom={16}
               />
               <MapBoundsUpdater bounds={mapBounds} />
               
